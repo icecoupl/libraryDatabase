fd5251bf28d2401ed7dc4f9542caedbc05e31140
Gene Kim <kimgene00@gmail.com>
2024-04-27 15:04:55 -0400

    allowed host update

--- cs348db/settings.py
+++ cs348db/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e%624w$+fw4ygo0#n7s_z37pyt!4!a43e&sm(d++*auadsasmz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.compute-1.amazonaws.com']
 
 
 # Application definition
